c75a1e2febb7cf3338835cc7661037cfe0641cf7
Caio <caio.roma@treeinova.com.br>
2026-04-10 16:35:40 -0300

    Robô rodando corretamente em formato debug

--- Automation.Robots/NFRobot.cs
+++ Automation.Robots/NFRobot.cs
@@ -1,4 +1,5 @@
 ﻿using Automation.Core;
+using Automation.Infrastructure.Security;
 using Microsoft.Playwright;
 using Serilog;
 
--- Automation.Worker/Program.cs
+++ Automation.Worker/Program.cs
@@ -1,4 +1,3 @@
-using Automation.Infrastructure.Security;
 using Automation.Infrastructure.Browser;
 using Automation.Worker;
 using Serilog;
@@ -8,38 +7,6 @@ var basePath = @"C:\AutomaFiscal";
 Directory.CreateDirectory(basePath);
 Directory.CreateDirectory(Path.Combine(basePath, "logs"));
 Directory.CreateDirectory(Path.Combine(basePath, "downloads"));
-
-if (args.Length > 0 && args[0].ToLower() == "setup")
-{
-    Console.WriteLine("=== Setup de Credenciais AutomaFiscal ===");
-
-    var cred = new CredentialService();
-
-    Console.Write("Usuário: ");
-    var usuario = Console.ReadLine();
-
-    string senha;
-    string confirmacao;
-
-    do
-    {
-        Console.Write("Senha: ");
-        senha = LerSenhaOculta();
-
-        Console.Write("Confirmar Senha: ");
-        confirmacao = LerSenhaOculta();
-
-        if (senha != confirmacao)
-            Console.WriteLine("Senhas não conferem. Tente novamente.");
-    }
-    while (senha != confirmacao);
-
-    cred.Save("usuario_teste", usuario!);
-    cred.Save("senha_teste", senha);
-
-    Console.WriteLine("Credenciais salvas com sucesso!!!");
-    return;
-}
 
 Log.Logger = new LoggerConfiguration()
     .WriteTo.File(
@@ -67,30 +34,4 @@ catch (Exception ex)
 finally
 {
     Log.CloseAndFlush();
-}
-
-static string LerSenhaOculta()
-{
-    var senha = string.Empty;
-    ConsoleKeyInfo key;
-
-    do
-    {
-        key = Console.ReadKey(true);
-
-        if (key.Key != ConsoleKey.Backspace && key.Key != ConsoleKey.Enter)
-        {
-            senha += key.KeyChar;
-            Console.Write("*");
-        }
-        else if (key.Key == ConsoleKey.Backspace && senha.Length > 0)
-        {
-            senha = senha[0..^1];
-            Console.Write("\b \b");
-        }
-    }
-    while (key.Key != ConsoleKey.Enter);
-
-    Console.WriteLine();
-    return senha;
 }
